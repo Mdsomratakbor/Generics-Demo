ce474367df0c37ec3004bfa03d36e461133a9258
Mdsomratakbor <somrat876@gmail.com>
2020-07-02 20:10:42 +0600

    Two method add

--- GenericsDemo/Models/Person.cs
+++ GenericsDemo/Models/Person.cs
@@ -8,5 +8,9 @@ namespace GenericsDemo.Models
 {
     public class Person
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool IsAlive { get; set; }
     }
+
 }
---
+++ GenericsDemo/WithoutGenerics/OriginalTextFileProcessor.cs
@@ -0,0 +1,45 @@
+﻿using GenericsDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsDemo.WithoutGenerics
+{
+    public class OriginalTextFileProcessor
+    {
+        public static List<Person> LoadPeople(string filePath)
+        {
+            List<Person> output = new List<Person>();
+            Person p;
+            dynamic lines = File.ReadAllLines(filePath).ToList();
+
+            // Remove the header row
+            lines.RemoveAt(0);
+            foreach (var line in lines)
+            {
+                dynamic values = line.Split(',');
+                p = new Person();
+                p.FirstName = values[0];
+                p.IsAlive = values[1];
+                p.LastName = values[2];
+                output.Add(p);
+            }
+            return output;
+        }
+
+        public static void SavePepole(List<Person> people, string filePath)
+        {
+            List<string> lines = new List<string>();
+            // Add a header row
+            lines.Add("FirstName, IsAlive, LastName");
+            foreach (var p in people)
+            {
+                lines.Add($"{p.FirstName}, {p.IsAlive},{p.LastName}");
+            }
+            File.WriteAllLines(filePath,lines);
+        }
+    }
+}
